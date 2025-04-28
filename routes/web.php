685514ab4305408118ac1e18159509664b863185
M. Haykal <haykalmuhammad456@gmail.com>
2025-04-28 10:10:26 +0700

    Perapihan folder

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\UsersController;
+use App\Http\Controllers\Admin\CategorysController;
+use App\Http\Controllers\Admin\ItemsController;
+use App\Http\Controllers\Admin\BorrowsController;
+use App\Http\Controllers\Auth\AuthController;
+use App\Http\Middleware\AdminMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +21,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
